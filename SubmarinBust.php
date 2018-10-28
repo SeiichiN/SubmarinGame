243e797a1717ea318e6136b9e41c4aaf2f531b15
SeiichiN <billie175@gmail.com>
2018-10-29 07:17:53 +0900

    SubmarinBust.phpwo wo class ni kaeta

--- SubmarinBust.php
+++ SubmarinBust.php
@@ -3,106 +3,135 @@ require_once("mylib.php");
 require_once("Submarin.php");
 require_once("GameHelper.php");
 
+class SubmarinBust {
 
+    private $submarinList = array();
+    private $subNum = 0;
+    private $numOfGuess = 0;
 
-function makeSubmarin($sub) {
-    static $i = 0;
-    $name = ["Papiyon", "SeaTiger", "Dragon"];
+    public function __construct() {
+        // Grid（マス目）を初期化する。
+        GameHelper::initGrid();
+
+        $sub1 = $this->makeSubmarin(new Submarin());
+        echo "sub1:" . $sub1->getName(), "\n";
+        $sub2 = $this->makeSubmarin(new Submarin());
+        echo "sub2:" . $sub2->getName(), "\n";
+        $sub3 = $this->makeSubmarin(new Submarin());
+        echo "sub3:" . $sub3->getName(), "\n";
+
+        array_push($this->submarinList, $sub1);
+        array_push($this->submarinList, $sub2);
+        array_push($this->submarinList, $sub3);
+    }
+
+    public function getSubmarinList() {
+        return $this->submarinList;
+    }
+
+    public function getNumOfGuess() {
+        return $this->numOfGuess;
+    }
     
-    // 1つめの潜水艦
-    $location = GameHelper::placeSubmarin(3);
-    $sub->setLocationCells($location);
-    $sub->setName($name[$i]);
-    $i++;
+    public function makeSubmarin($sub) {
+        $name = ["Papiyon", "SeaTiger", "Dragon"];
+        
+        // 1つめの潜水艦
+        $location = GameHelper::placeSubmarin(3);
+        $sub->setLocationCells($location);
+        $sub->setName($name[$this->subNum]);
+        $this->subNum++;
+        
+        // var_dump($location);
+        return $sub;
+    }
     
-    // var_dump($location);
-    return $sub;
-}
+    public function checkUserGuess($guess) {
+        
+        $this->numOfGuess++;
 
-function checkUserGuess($guess) {
-    global $numOfGuess;
-    global $submarinList;
-    
-    $numOfGuess++;
+        $guess = strtoupper($guess);
+        
+        $result = "失敗";
+        foreach ($this->submarinList as $idx=>$sub) {
+            // echo "submarin : " . $sub->getName() . "\n";
+            $result = $sub->checkYourself($guess);
 
-    $guess = strtoupper($guess);
-    
-    $result = "失敗";
-    foreach ($submarinList as $idx=>$sub) {
-        // echo "submarin : " . $sub->getName() . "\n";
-        $result = $sub->checkYourself($guess);
+            if ($result === "命中") break;
+            if ($result === "撃沈") {
+                $result = $sub->getName() . ":" . $result;
+                array_splice($this->submarinList, $idx, 1);
+                break;
+            }
+        }
+        return $result;
+    }
 
-        if ($result === "命中") break;
-        if ($result === "撃沈") {
-            $result = $sub->getName() . ":" . $result;
-            array_splice($submarinList, $idx, 1);
-            break;
+    public function gameStart() {
+        $msg = "このゲームの目的は3隻の潜水艦を撃沈することです。";
+        $msg .= "潜水艦の潜む海域は、7 X 7 の 49 のマス目です。";
+        $msg .= "横に A 〜 G の7つ。縦に 1 〜 7 の7つです。";
+        $msg .= "各艦は、3ﾏｽの大きさをもっています。";
+        $msg .= "d4 とか B3 とかいうふうにマス目を指定してください。";
+        $msg .= "攻撃回数20回以下だと優秀です。では、頑張ってください。";
+        return $msg;
+    }
+
+    public function gameEnd() {
+        $i = $this->numOfGuess;
+
+        $msg = "全隻が撃沈されました。";
+        $msg .= "あなたの攻撃回数は、$i でした。";
+
+        return $msg;
+    }
+
+    /**
+     * デバッグ用に各潜水艦の位置を表示する
+     */
+    public function openLocation() {
+        global $submarinList;
+
+        foreach ($submarinList as $idx=>$sub) {
+            echo $sub->getName() . " : ";
+            echo $sub->getLocationCells() .  "\n";
         }
     }
-    return $result;
+
 }
 
-function gameStart() {
-    $msg = "このゲームの目的は3隻の潜水艦を撃沈することです。";
-    $msg .= "潜水艦の潜む海域は、7 X 7 の 49 のマス目です。";
-    $msg .= "横に A 〜 G の7つ。縦に 1 〜 7 の7つです。";
-    $msg .= "各艦は、3ﾏｽの大きさをもっています。";
-    $msg .= "d4 とか B3 とかいうふうにマス目を指定してください。";
-    $msg .= "攻撃回数20回以下だと優秀です。では、頑張ってください。";
-    return $msg;
-}
+/* $submarinList = array();
+ * 
+ * // Grid（マス目）を初期化する。
+ * GameHelper::initGrid();
+ * 
+ * $sub1 = makeSubmarin(new Submarin());
+ * // echo "sub1:" . $sub1->getName(), "\n";
+ * $sub2 = makeSubmarin(new Submarin());
+ * // echo "sub2:" . $sub2->getName(), "\n";
+ * $sub3 = makeSubmarin(new Submarin());
+ * //echo "sub3:" . $sub3->getName(), "\n";
+ * 
+ * array_push($submarinList, $sub1);
+ * array_push($submarinList, $sub2);
+ * array_push($submarinList, $sub3);
+ * 
+ * // openLocation();
+ * 
+ * $numOfGuess = 0;*/
 
-function gameEnd() {
-    global $numOfGuess;
+$game = new SubmarinBust();
 
-    $msg = "全隻が撃沈されました。";
-    $msg .= "あなたの攻撃回数は、{$numOfGuess} でした。";
+echo $game->gameStart(), "\n";
 
-    return $msg;
-}
-
-/**
- * デバッグ用に各潜水艦の位置を表示する
- */
-function openLocation() {
-    global $submarinList;
-
-    foreach ($submarinList as $idx=>$sub) {
-        echo $sub->getName() . " : ";
-        echo $sub->getLocationCells() .  "\n";
-    }
-}
-
-$submarinList = array();
-
-// Grid（マス目）を初期化する。
-GameHelper::initGrid();
-
-$sub1 = makeSubmarin(new Submarin());
-// echo "sub1:" . $sub1->getName(), "\n";
-$sub2 = makeSubmarin(new Submarin());
-// echo "sub2:" . $sub2->getName(), "\n";
-$sub3 = makeSubmarin(new Submarin());
-//echo "sub3:" . $sub3->getName(), "\n";
-
-array_push($submarinList, $sub1);
-array_push($submarinList, $sub2);
-array_push($submarinList, $sub3);
-
-// openLocation();
-
-$numOfGuess = 0;
-
-echo gameStart(), "\n";
-
-while (!empty($submarinList)) {
+while (!empty($game->getSubmarinList())) {
     echo "A1 〜 G7 までのセルを指定 > ";
     $myGuess = trim(fgets(STDIN));
-    $result = checkUserGuess($myGuess);
+    $result = $game->checkUserGuess($myGuess);
     echo $result, "\n";
 }
 
-echo gameEnd(), "\n";
+echo $game->gameEnd(), "\n";
 
 /* $myGuess = "B3";
  * $myResult = $sub1->checkYourself($myGuess);
